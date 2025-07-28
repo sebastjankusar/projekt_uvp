662ff3d3140c3900c2e55cd0a9562d1688a62f7a
Sebastjan Kušar <sebastjan123kusar@gmail.com>
2025-07-28 13:37:35 +0200

    popravil funkcijo za visja_ocena

--- analiza_funkcije.py
+++ analiza_funkcije.py
@@ -3,9 +3,9 @@ import pandas as pd
 def doloci_visjo_oceno(vrstica):
     if pd.isna(vrstica["ocena_imdb"]) or pd.isna(vrstica["ocena_rotten_tomatoes"]):
         return "ni_podatkov"
-    elif vrstica["ocena_imdb"] == vrstica["ocena_rotten_tomatoes"]:
+    elif vrstica["ocena_imdb"] == (vrstica["ocena_rotten_tomatoes"] / 10):
         return "enako"
-    elif vrstica["ocena_imdb"] > vrstica["ocena_rotten_tomatoes"]:
+    elif vrstica["ocena_imdb"] > (vrstica["ocena_rotten_tomatoes"] / 10):
         return "imdb"
     else:
         return "rotten_tomatoes"
